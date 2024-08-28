7c93e67113fd1218b440a87ccaa5f8c1265c75b6
IdirX69 <adil.idirr@gmail.com>
2024-08-28 18:02:33 +0200

    Basic fetch

--- javascript/index.js
+++ javascript/index.js
@@ -15,3 +15,22 @@ let text = "Check";
 document.getElementById("changeTextBtn").addEventListener("click", () => {
   document.getElementById("myText").textContent = "Hello," + text + "!";
 });
+
+// Task 3
+
+const fetchTodosList = async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/?_limit=5"
+  );
+  if (response.ok) return response.json();
+  throw new Error("something was wrong");
+};
+
+fetchTodosList().then((data) => {
+  const todoList = document.getElementById("todo-list");
+  data.map((todo) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = todo.title;
+    todoList.appendChild(listItem);
+  });
+});
